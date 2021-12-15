c48ed87df8ef47172fe9d8b12b7f4226e4609265
David Atkinon <david.atkinson@netzero.com>
2021-12-15 17:14:36 -0600

    initial

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Enums/FelipeState.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.Enums;
+
+public enum FelipeState {
+    PRE_INITIALIZE,
+    INITIALIZE,
+    LOAD,
+    READY_TO_PIVOT,
+    ALLIANCE_LEFT,
+    ALLIANCE_RIGHT,
+    SHARED_LEFT,
+    SHARED_RIGHT,
+    RELOAD
+
+
+}
