17ead023510031de3bb0f5985d64e41f219968e8
David Atkinon <davidatkinson549@gmail.com>
2022-01-23 11:37:36 -0600

    fixed spelling and added mechanical reset

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/JuanStateMachineExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/JuanStateMachineExample.java
@@ -37,10 +37,12 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
+import org.firstinspires.ftc.teamcode.Enums.LiftPosition;
+
 
 @TeleOp(name="Juan State Machine Example", group="Linear Opmode")
 //@Disabled
-public class JuamStateMachineExample extends LinearOpMode {
+public class JuanStateMachineExample extends LinearOpMode {
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
@@ -52,12 +54,15 @@ public class JuamStateMachineExample extends LinearOpMode {
     private static final double     LIFT_DIST_PER_REV           = 42 * Math.PI / 25.4; // inches of lift per motor rev (42mm  winch pulley)
     public static final double      TICKS_PER_LIFT_IN           = TICKS_PER_MOTOR_REV / LIFT_DIST_PER_REV ; // 100 and change
 
-    private static final double     JUANLPARTIAL                = 5.5; // inches
+    private static final double     JUAN_HEIGHT_PARTIAL         = 5.5; // inches
 
-    private static final double     JUANLIFTSPEED_UP            = 0.9; // power
-    private static final double     JUANLIFTSPEED_DOWN          = -0.2; // power
-    private static final double     JUANLIFTSPEED_HOLD          = 0.2;
-    private static final double     maxTeleopTimeOut            = 200; // seconds to automotically shut down teleop after to protect motors
+    private static final double     JUAN_SPEED_UP               = 0.9; // power
+    private static final double     JUAN_SPEED_DOWN             = -0.2; // power
+    private static final double     JUAN_SPEED_HOLD             = 0.2;
+    private static final double     TELEOP_TIME_OUT             = 122; // seconds - Automatically shuts down teleop to prevent motor damage (for practice sessions)
+
+    private LiftState mliftstate; // "m" = class variable that all methods can share. Not a local variable
+    private ArmState marmstate;
 
     private enum LiftState {
         LIFT_DOWN,
@@ -65,6 +70,7 @@ public class JuamStateMachineExample extends LinearOpMode {
         LIFT_HOLD,
         LIFT_IDLE,
         MANUAL,
+        LIFT_MECH_RESET,
         UNKNOWN
     }
 
@@ -75,13 +81,12 @@ public class JuamStateMachineExample extends LinearOpMode {
         UNKNOWN
     }
 
-    private LiftState mliftstate; // m = member of the class as opposed to a local variable inside the "runOpmoode" method
-    private ArmState marmstate;
+
 
     @Override
     public void runOpMode() {
 
-        int juanPosition; // local variable that onlt works inside "runOpmode"
+        int juanPosition; // local variable that only works inside "runOpmode" it is not shared to other methods
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -99,23 +104,19 @@ public class JuamStateMachineExample extends LinearOpMode {
         julio.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
         marmstate = ArmState.UNKNOWN;
 
+        juanMechanicalReset();// make sure lift is all the way down before starting
 
         waitForStart();
         runtime.reset();
 
         // run until the end of the match (driver presses STOP)
-        while (opModeIsActive() &&  teleopTimer.seconds() < maxTeleopTimeOut) {
+        while (opModeIsActive() &&  teleopTimer.seconds() < TELEOP_TIME_OUT  ) {
 
             double juanpower = -gamepad1.left_stick_y;
             double juliopower = gamepad1.right_stick_x;
 
-            // Show the elapsed game time and wheel power.
-            //telemetry.addData("Status", "Run Time: " + runtime.toString());
-            //telemetry.addData("Motors", "juan (%.2f), julio (%.2f)", juanpower, juliopower);
-            //telemetry.update();
-
-///////////////Driver initiated state transition via the game pad//////////////////////////
-
+/////////////// Game Pad 1 Assignments /////////////////////////
+             // Button names and positions
             //        y
             //      x   b
             //        a
@@ -127,7 +128,7 @@ public class JuamStateMachineExample extends LinearOpMode {
             if (gamepad1.a) {
                 mliftstate = LiftState.LIFT_DOWN;
             }
-
+            // by moving the left joystick in either direction the driver takes manual control.
             if (Math.abs(gamepad1.left_stick_y) > 0.25) {
                 mliftstate = LiftState.MANUAL;
             }
@@ -140,19 +141,19 @@ public class JuamStateMachineExample extends LinearOpMode {
 
                 case LIFT_UP:
 
-                    if  (juanPosition < JUANLPARTIAL  *  TICKS_PER_LIFT_IN ) // this prevents short power up of motore each time button is pressed.
-                    {juan.setPower(Math.abs(JUANLIFTSPEED_UP));
+                    if  (juanPosition <JUAN_HEIGHT_PARTIAL  *  TICKS_PER_LIFT_IN ) // this prevents short power up of motore each time button is pressed.
+                    {juan.setPower(Math.abs(JUAN_SPEED_UP ));
                     }
                     // The exit condition for LIFT_UP is reaching the target height - no operator input needed
                     // If the driver presses the DOWN button it would also change the state
-                    if( juanPosition >= JUANLPARTIAL  *  TICKS_PER_LIFT_IN  ){
+                    if( juanPosition >= JUAN_HEIGHT_PARTIAL  *  TICKS_PER_LIFT_IN  ){
                         mliftstate = LiftState.LIFT_HOLD;
                     }
 
                     break;
 
                 case LIFT_DOWN: //
-                    juan.setPower(JUANLIFTSPEED_DOWN);
+                    juan.setPower(JUAN_SPEED_DOWN);
                     // The exit condition for LIFT_DOWN is reaching a very low lift position - no operator input needed
                     if(juan.getCurrentPosition() < 20){
                         mliftstate = LiftState.LIFT_IDLE;
@@ -161,7 +162,7 @@ public class JuamStateMachineExample extends LinearOpMode {
                     break;
 
                 case LIFT_HOLD: // Power high enough to prevent lift from dropping but not high enough to lift it.
-                    juan.setPower(Math.abs(JUANLIFTSPEED_HOLD));
+                    juan.setPower(Math.abs(JUAN_SPEED_HOLD));
                     // There is no exit condition defined in this case. Exisitng this state requires the driver to press up or down.
                     // Pressing up will just put the state back in hold f the lift is up. You would need to press DOWN.
                     break;
@@ -192,5 +193,31 @@ public class JuamStateMachineExample extends LinearOpMode {
 
 
         }
+    }
+
+    public void juanMechanicalReset(){
+        mliftstate = LiftState.MANUAL.LIFT_MECH_RESET; // we know the lift is at zero here.
+        juan.setPower(JUAN_SPEED_DOWN);
+        runtime.reset();
+        // opmode is not active during init so take that condition out of the while loop
+        while ((runtime.seconds() < 2.0)) {
+
+            telemetry.addData("Lift State is",mliftstate);
+            telemetry.update();
+        }
+        // set everything back the way is was before reset so encoders can be used
+        juan.setPower(0); // stop motor after time expires
+        sleep(200);// need to pause before resetting the encoder.
+        // The spring on the goBilda cascade lift allows the motor to keep turning when the lift is
+        // all the way at the bottom. When power is shut off the spring turns the motor in the "lift"
+        // direction. THe sleep lets the motor come to rest before encoder reset. Otherwise the ticks
+        // will not be be approx zero when the lift is physically at its lowerst point.
+        juan.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        juan.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);// make sure to add this after reset to switch out of encoder mode
+        mliftstate = LiftState.MANUAL.LIFT_IDLE;
+
+        telemetry.addData("Lift State is",mliftstate);
+        telemetry.update();
+
     }
 }
